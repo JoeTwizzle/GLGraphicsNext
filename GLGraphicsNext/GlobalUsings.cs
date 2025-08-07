6864a922ec0dfea5fdcf874736aae213270e1534
JoeTwizzle <the3.14159268483279@gmail.com>
2025-08-07 11:16:20 +0200

    Update GlobalUsings.cs

--- GLGraphicsNext/GlobalUsings.cs
+++ GLGraphicsNext/GlobalUsings.cs
@@ -4,5 +4,4 @@ global using Mat4 = System.Numerics.Matrix4x4;
 global using Vec4 = System.Numerics.Vector4;
 
 
-//TODO: Compressed Texture Support, 
 //Enhancements: Better documentation, Bindless textures, Filtering params for Textures & Texture Views, All constructors, all uint only functions
